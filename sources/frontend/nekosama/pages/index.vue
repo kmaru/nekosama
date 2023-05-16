622a75a039550bf54a0273852250b7e721b095f1
Kazuhide Maruyama <k-maru@archway.co.jp>
2023-05-16 16:32:51 +0900

    initial commit

---
+++ sources/frontend/nekosama/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <h1>Index</h1>
+    <p>{{ data }}</p>
+</template>
+<script setup>
+const data = await $fetch('/api/hello')
+</script>
---
+++ sources/frontend/nekosama/server/api/hello.ts
@@ -0,0 +1,1 @@
+export default defineEventHandler(() => `${new Date().toLocaleString()} Hello, world!`);
